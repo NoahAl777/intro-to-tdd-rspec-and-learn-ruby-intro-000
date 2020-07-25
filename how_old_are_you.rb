08aa4f5a2e5d942baa5b01aa4cae3ebae18b8dcd
Noah Alston <crashlive112@gmail.com>
2020-07-25 04:57:41 +0000

    Automatically backed up by Learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -3,7 +3,7 @@ require_relative './current_age_for_birth_year.rb'
 puts "What year were you born?"
 birth_year = gets.to_i
 def current_age_for_birth_year(birth_year)
-
+  Time.now.year - birth_year
 end
 users_age = current_age_for_birth_year(birth_year)
 
